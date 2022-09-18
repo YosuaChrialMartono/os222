0c4ce5e13e752c5fd4e58dd7572ab467bb3f01fb
kkoep <yosua.chrial@ui.ac.id>
2022-09-18 11:57:21 +0700

    Commit week 2 - 2 New username  Changes to be committed: 	modified:   SHA256SUM 	modified:   SHA256SUM.asc 	modified:   myW02.tar.bz2.asc 	modified:   mypubkey.txt 	modified:   myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="516DF61A3B8DC15A"
+REC2="A83763E1F1CD1367"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
